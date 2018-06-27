249a910917ed7165e38978d0b717eb3630bcb59f
wqz <1280356689.qq.com>
2018-06-27 19:39:51 +0800

    第二次

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Pay;
 use Illuminate\Http\Request;
+include ('PayLib.php');
 
 class PayController extends Controller
 {
@@ -46,7 +47,6 @@ class PayController extends Controller
     public function enter(Request $request)
     {
         $address = $_SERVER['HTTP_HOST'];
-        //var_dump($address);die;
         $this->validate($request,
             [
                 'name' => 'required',
@@ -61,42 +61,48 @@ class PayController extends Controller
         );
         $order_id = date('YmdHis', time());
         $user_id=uniqid();
-        $param = [
-            "oid_partner" =>'201709141355751809',
-            'time_order' => $order_id,
-            'no_order' =>'201610270213',
-            'pay_type' => '32',
-            'money_order' => $request->money,
-            'name_goods' => '玩具娃娃',
-            'info_order' => '不错的产品',
+        $parameter = array(
+            "oid_partner" => "201709141355751809",
+            "time_order" => $order_id,
+            "no_order"	=>"201610270213999",
+            "notify_url"	=>"http://www.baidu.com/",
+            "pay_type"	=> "48",
+            "money_order"	=>100,
+            "name_goods"	=> "sssss",
             "user_id"	=> $user_id,
-            "sign_type" => 'MD5',
-            ];
-        //$arr=http_build_query($param);
-        //var_dump($param);die;
-//          var_dump(http_build_query($param));die;
-        $code=http_build_query($param);
-        $fix=$code.'hs12ll29g';
-        //var_dump($fix);die;
-        $code=md5($fix);
-        //var_dump($code);die;
-        $para = [
-            'name' => $request->name,
-            'oid_partner' => '201709141355751809',
-            'notify_url' => 'http://$address/notify.php',
-            'user_id' => $user_id,
-            'sign_type' => 'MD5',
-            'sign' =>$code,
-            'no_order' => 201610270213,
-            'time_order' => $order_id,
-            'money_order' => $request->money,
-            'info_order' => '不错的产品',
-            'name_goods' => '玩具娃娃',
-            'pay_type' => '32',
-        ];
-        //var_dump($para);die;
-        $code1=http_build_query($para);
+            "sign_type" => "MD5",
+//
+//            'money_order' => "100",
+//            'name_goods' => 'sssss',
+//            'no_order' => "201610270213999", 'notify_url' => "http://www.baidu.com/",
+//            'oid_partner' => "201709141355751809",
+//            'pay_type' => '48',
+//            'sign_type' => 'MD5',
+//            'time_order' => $order_id,
+//            'user_id' => $user_id,
+        );
 
+        $parameter = paraFilter($parameter);
+        $parameter = argSort($parameter);
+//        $signStr = "";
+        $signStr = createLinkstring($parameter);
+        $merchant_md5_key='hs12ll29g';
+        var_dump($signStr);die;
+        $code = md5Sign($signStr, $merchant_md5_key);
+
+        $para = [
+            'money_order' => 100,
+            'name_goods' => 'sssss',
+            'no_order' => "201610270213999",
+            'notify_url' => "http://www.baidu.com/",
+            'oid_partner' => "201709141355751809",
+            'pay_type' => '48',
+            'sign_type' => 'MD5',
+            'time_order' => $order_id,
+            'user_id' => $user_id,
+            'sign' =>$code,
+        ];
+        $code1=http_build_query($para);
         return redirect("http://yiapi.lianlianspc.com/gateway/yigateway?{$code1}",302);
     }
 }
---
+++ app/Http/Controllers/PayLib.php
@@ -0,0 +1,99 @@
+<?php
+/* *
+ * 支付接口公用函数
+ * 详细：该类是请求、通知返回两个文件所调用的公用函数核心处理文件
+ * 说明：
+ * 以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
+ */
+
+/**
+ * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+ * @param $para 需要拼接的数组
+ * return 拼接完成以后的字符串
+ */
+function createLinkstring($para) {
+    $arg  = "";
+    while (list ($key, $val) = each ($para)) {
+        $arg.=$key."=".$val."&";
+    }
+    //去掉最后一个&字符
+    $arg = substr($arg,0,count($arg)-2);
+    //file_put_contents("log.txt","转义前:".$arg."\n", FILE_APPEND);
+    //如果存在转义字符，那么去掉转义
+    if(get_magic_quotes_gpc()){$arg = stripslashes($arg);}
+    //file_put_contents("log.txt","转义后:".$arg."\n", FILE_APPEND);
+    return $arg;
+}
+
+/**
+ * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串，并对字符串做urlencode编码
+ * @param $para 需要拼接的数组
+ * return 拼接完成以后的字符串
+ */
+function createLinkstringUrlencode($para) {
+    $arg  = "";
+    while (list ($key, $val) = each ($para)) {
+        $arg.=$key."=".urlencode($val)."&";
+    }
+    //去掉最后一个&字符
+    $arg = substr($arg,0,count($arg)-2);
+
+    //如果存在转义字符，那么去掉转义
+    if(get_magic_quotes_gpc()){$arg = stripslashes($arg);}
+
+    return $arg;
+}
+
+/**
+ * 除去数组中的空值和签名参数
+ * @param $para 签名参数组
+ * return 去掉空值与签名参数后的新签名参数组
+ */
+function paraFilter($para) {
+    $para_filter = array();
+    while (list ($key, $val) = each ($para)) {
+        if($key == "sign" || $val == "")continue;
+        else	$para_filter[$key] = $para[$key];
+    }
+    return $para_filter;
+}
+
+/**
+ * 对数组排序
+ * @param $para 排序前的数组
+ * return 排序后的数组
+ */
+function argSort($para) {
+    ksort($para);
+    // var_dump($para);
+    // exit();
+    reset($para);
+    return $para;
+}
+
+//格式化时间戳
+function local_date($format, $time = NULL){
+    if ($time === NULL)
+    {
+        $time = gmtime();
+    }
+    elseif ($time <= 0)
+    {
+        return '';
+    }
+    return date($format, $time);
+}
+
+/**
+ * 签名字符串
+ * @param $prestr 需要签名的字符串
+ * @param $key 私钥
+ * return 签名结果
+ */
+function md5Sign($prestr, $key) {
+    $prestr = $prestr . $key;
+    return md5($prestr);
+}
+
+
+?>
