5fdd374a81ee9f1578cf48309e55b8341ded5d74
wqz <1280356689.qq.com>
2018-07-02 18:55:46 +0800

    1

--- resources/views/pay/index.blade.php
+++ resources/views/pay/index.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-offset-2 col-md-8">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h5>快捷支付</h5>
+                <h5>支付选择</h5>
             </div>
             <div class="panel-body">
                 <form method="POST" action="{{ route('inters') }}" enctype="multipart/form-data">
@@ -17,16 +17,21 @@
                     <div class="form-group" >
                         <label for="inputPassword3" class="col-sm-2 control-label">金额：</label>
                         <div class="col-sm-4">
-                            <input type="text" name="money" class="form-control" value="{{ old('money') }}">
+                            <input type="text" name="money_order" class="form-control" value="{{ old('money_order') }}">
 
                     </div>
                     </div>
                     <div style="margin:10px 30px">
 
                         <label class="radio-inline">
-                            <input type="radio" name="action" id="inlineRadio1" value="23">支付宝扫码
+                            <input type="radio" name="pay_type" id="inlineRadio1" value="23">支付宝扫码
                         </label>
-
+                        <label class="radio-inline">
+                            <input type="radio" name="pay_type" id="inlineRadio1" value="11"> 网银支付
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="pay_type" id="inlineRadio1" value=""> 代付
+                        </label>
                     </div>
 
                     {{csrf_field()}}
